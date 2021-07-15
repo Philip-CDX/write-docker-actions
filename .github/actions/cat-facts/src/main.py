f081e145ea81a2fc957718ac7b571f06f3de1f9d
Philip-CDX <86932285+Philip-CDX@users.noreply.github.com>
2021-07-15 13:30:58 +1200

    Update main.py

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -1,27 +1,37 @@
+import json
+
 import requests
 import random
 import sys
 
 # Make an HTTP GET request to the cat-fact API
-cat_url = "https://catfact.ninja/facts?limit=1"
+cat_url = "https://catfact.ninja/facts"
+
+
 r = requests.get(cat_url)
-r_obj_list = r.json()
 
-# Create an empty list to store individual facts in
-# This will make it easy to select a random one later
-fact_list = []
+r_obj_list: object = r.json()
 
-# Add the "text" of every object into the fact_list list
-for fact in r_obj_list:
-    fact_list.append(fact["text"])
+random_fact = r_obj_list.get('data')[0].get('fact')
 
-# Select a random fact from the fact_list and return it
-# into a variable named random_fact so we can use it
-def select_random_fact(fact_arr):
-    return fact_arr[random.randint(0, len(fact_list)+1)]
-
-random_fact = select_random_fact(fact_list)
-
+#
+# # Create an empty list to store individual facts in
+# # This will make it easy to select a random one later
+# fact_list = []
+#
+# # Add the "text" of every object into the fact_list list
+# for fact in r_obj_list:
+#     fact_list.append(fact["text"])
+#
+#
+# # Select a random fact from the fact_list and return it
+# # into a variable named random_fact so we can use it
+# def select_random_fact(fact_arr):
+#     return fact_arr[random.randint(0, len(fact_list) + 1)]
+#
+#
+# random_fact = select_random_fact(fact_list)
+#
 # Print the individual randomly returned cat-fact
 print(random_fact)
 
